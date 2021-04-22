e9f617af439681f12fec8b05b888dc5a30ca50dc
sharlq <shehabakhader@gmail.com>
2021-04-22 11:28:03 +0300

    the end of this working session

--- src/components/materialUIStyling.jsx
+++ src/components/materialUIStyling.jsx
@@ -1,7 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
  const useStyles = makeStyles((theme)=>({
     root: {
-        margin: "0 auto 0 auto",
+      margin: "0 auto 0 auto",
       minWidth: 275,
       maxWidth: 400
     },
