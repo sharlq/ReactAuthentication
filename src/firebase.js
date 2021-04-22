06a9485a8316a7f1a3ac9e85cd4c31e8745e65d9
sharlq <shehabakhader@gmail.com>
2021-04-22 03:20:01 +0300

    more organizing and solving some problems

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,16 @@
 import firebase from "firebase/app"
 import "firebase/auth"
-
+// note here when there is problem in the api key when we imported this into the context the whole app didnt render remmember this for future debuging with other projects
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_APIKEY ,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN ,
-    projectId: process.env.REACT_APP_PROJECTID ,
-    storageBucket: process.env.REACT_APP_STRONGBUCKET ,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID ,
-    appId: process.env.REACT_APP_APPID
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY ,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN ,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID ,
+    storageBucket: process.env.REACT_APP_FIREBASE_STRONGBUCKET ,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID ,
+    appId: process.env.REACT_APP_FIREBASE_APPID
 })
+
+
 
 export const auth = app.auth()
 export default app
